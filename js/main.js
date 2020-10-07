f7660cd2507a3b66a11f2fe8a4cadfea9e768b6e
maysaa shajrawi <maysaashajrawi@gmail.com>
2020-10-07 23:56:59 +0300

    DonesearchByname

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
 // array of object for human-developement books
 
-var humanDevelopement = [{name:"مميز بالأصفر",
+var humanDevelopement = [{id:1,
+                          name:"مميز بالأصفر",
                           img:"img/A-2.jpg",
                           description:'يمتعنا "إتش. جاكسون براون" فى كتابه مميز بالأصفر" بمجموعة من القصص والاقتباسات التى تدل على فهم عميق لأسس الحياة الطيبة، فهذا الكتاب يعد مقرراً تعليمياً مختصراً فى كيفية العيش بحكمة والاختيار بذكاء، يسلط فيه الكاتب الضوء على أهمية التصرف بلطف وكرم وإحسان مع الآخرين، وتوطيد علاقات قوية راسخة مع الأشخاص الذين نحبهم، واختيار توجهات ذهنية تساعدنا أثناء عيش ساعات الحياة اليومية العادية، واكتشاف معنى الرضا والإشباع النابعين من تقدير المتع البسيطة والاستمتاع بها.',
                           url:"https://archive.org/details/www.nasadrar.com/mode/2up"
                             },
-                            {name:"عشرة أمور تمنيت لو عرفتها قبل دخولي الجامعة",
+                            {id:2,
+                              name:"عشرة أمور تمنيت لو عرفتها قبل دخولي الجامعة",
                             img:"img/A-3.jpg",
                             description:'يتطرق الدكتور "ياسر عبد الكريم بكار" في هذا الكتاب إلى كافه جوانب الحياة الجامعية لدى كل من الشاب والفتاة علميا ، أكاديميا، روحانيا، اجتماعيا، وحتى على الجانب العاطفي؛ فيبين بذلك كل جوانب الصعاب والمشاكل الحياتية الجامعية التي من شأن كل طالب مقبل على الجامعه الولوج فيها..',
                             url:"https://archive.org/details/shimaaali7557ss_yahoo"},
-                            {name:"نظرية الفستق",
+                            {id:3,
+                              name:"نظرية الفستق",
                             img:"img/A-7.jpg",
                             description:'كتاب سيغير طريقة تفكيرك وحكمك على الأشياء. يحتوي على مجموعة مقالات تتمحور غالبا عن تطوير الذات والبرمجة اللغوية العصبية وطرق التفكير والوعي والسلوك الإنساني. الكتاب يتضمن 65 موضوعا. كل منها تختوي على 5 صفحات وهو خاص بطرق التفكير والسلوك والحكم على الأشياء. كتاب تنمية ذاتيه بحت.',
                             url:"https://foulabook.com/ar/book/%D9%86%D8%B8%D8%B1%D9%8A%D8%A9-%D8%A7%D9%84%D9%81%D8%B3%D8%AA%D9%82-pdf"
                               },{
+                                id:5,
                                 name:"ابق قويا 365 يوم يالسنة",
                                 img:"img/A-1.jpg",
                                 description:"لقد لخصت ديمي لوفاتو الدروس التي تعلمتها خلال رحلتها في الحياة في هذا الكتاب وهي مجموعة مكونة من 365 يوما من أكثر أفكارها صراحة وشجاعة وتفاؤلا انها كلماتها الخاصة بالاضافة لمقتبسات ألهمتها وأيضا تأملات وأهداف خاصة بها هذا الكتاب يخاطب كل شخص في كل مكان في رحلته الخاصة لكل من يحتاج للراحة والالهام والدافع ليبقى قويا كل يوم",
                                 url:"https://archive.org/details/20191015_20191015_0502"
-                              },{
+                              },{id:6,
                                 name:"امرأة من طراز خاص",
                                 img:"img/A-4.jpg",
                                 description:"كتاب يخاطب المرأة الطموحة، عبر استعراضه لعادات خمس يرى بأهمية أن تلتفت إليهم كل من تطمح في ارتياد القمة . يبدأ بعادة المبادرة، مشددا على أن المرأة الناجحة لا بد وأن تتمتع بصفة الإقدام، تصنع حياتها، وتمهد دربها، دون أن تنتظر ما قد يمن به عليها شخصا ما . ثم يتحدث عن الثقة بالنفس، فالنجاح لا يقترب من شخص مزعزع الفكر، مهترئ الوجدان، الثقة بالنفس هي التي تدفع المرء منا إلى العمل والتميز والنجاح . والعادة الثالثة هي الذكاء الاجتماعي، عن المرأة ذات القلب الساحر، الذي يستحوذ على نواصي القلوب والعقول يحدثنا . العادة الرابعة هيي التخطيط للحياة، كيف تسيطر المرأة على حياتها، وتضع البرامج والخطط لتحقيق أفضل النتائج، سؤال يجيبك عنه الكتاب . والعادة الخامسة والأخيرة هي إدارة الأولويات، عن الوقت وأهميته وطرق الاستفادة منه",
                                 url:"https://archive.org/details/734specialwomentraz"
                               },{
+                                id:7,
                                 name:"العادات السبع للناس الأكثر فاعلية",
                                 img:"img/A-5.jpg",
                                 description:"الكتاب الاكثر مبيعا في أمريكا والعالم بيعت منه اكثر من 15 مليون نسخة كتاب يسبر أغوار الشخصية الانسانية ليخرج لنا بالعادات التي تنظم له سلوكه وتؤثر على حياته بفعالية كبيرة بشكل ايجابي .. هل تريد أن تكون ناجحا ؟؟ هل تريد قراءة كتاب يغير حياتك بشكل ايجابي؟؟ عليك بهذا الكتاب ..",
                                 url:"https://archive.org/details/contact_ikra_20190313_1311"
-                              },{
+                              },{id:8,
                                 name:"العادات السبع للناس الأكثر فاعلية",
                                 img:"img/A-5.jpg",
                                 description:"تتشابه الأمومة والأبوة مع أي جانب آخر من جوانب الحياة ، حيث أننا نستطيع أن نتقدم وننجح إذا حاولنا تحليل الخطأ الذي وقعنا فيه ، وأسباب وقوعه وعملنا على إصلاحه . فمثلاً إن كان هناك طفلة ترفض القيام بواجباتها المنزلية ، وتستمر في الرفض على الرغم من عدد المرات التي يطلب منها والداها فعله وإلحاحهما بقولهما ‘ افعليه ، افعليه ، افعليه ‘ . عليهم إيجاد أسلوب آخر في المعاملة ",
@@ -272,14 +277,39 @@ function createElement(array , type){
 
     for(var i = 0 ; i < array.length ; i++){
       obj=array[i];
-      console.log(array[i])
-             $('#'+type).append("<div class='col-md-3 col-sm-4'><div class='book text-center'> <img class='cover' src="+array[i].img+" alt='pic_1'><div class='text-book'><h4 class='title'>"+array[i].name+"</h4><p class='description'>"+array[i].description+"</p></div><a href="+array[i].url+" target='_blank'><button class='btn btn-success center'>Download</button></a></div></div>");
+      // console.log(array[i])
+      // htmlEntities(array[i].name);
+             $('#'+type).append("<div class='col-md-3 col-sm-4 col-xs-5'><div class='book text-center'> <img class='cover' src="+array[i].img+" alt='pic_1'><div class='text-book'><h4 class='title'>"+array[i].name+"</h4><p class='description'>"+array[i].description+"</p></div><a href="+array[i].url+" target='_blank'><button class='btn btn-success center'>Download</button></a></div></div>");
     }
 }
-$('#educational').addClass("show"); 
-$('#human-developement').addClass("show"); 
-$('#scientific').addClass("show"); 
-$('#historical').addClass("show"); 
+
+// function htmlEntities(str) {
+//   return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+// }
+var newArray=[];
+newArray.push(religious);
+newArray.push(humanDevelopement);
+newArray.push(historical);
+
+function searchFunc(){
+  var inputField = document.getElementById('searchInput').value;
+  // console.log(inputField)
+  for(var i=0;i<newArray.length;i++){
+    //pass every object in newArray
+  const index1 = newArray[i].findIndex(function(object , index){
+      if(object.name === inputField){
+        
+        console.log("hello");
+        $(".searchResult").append("<div class='col-md-3 col-sm-4 col-xs-5'><div class='book text-center'> <img class='cover' src="+object.img+" alt='pic_1'><div class='text-book'><h4 class='title'>"+object.name+"</h4><p class='description'>"+object.description+"</p></div><a href="+object.url+" target='_blank'><button class='btn btn-success center'>Download</button></a></div></div>");
+      }
+    });  
+  } 
+}
+
+
+
+
+
 
 
 
